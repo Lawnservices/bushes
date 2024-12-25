19716a1aaa697902078f1a864bbf35b2f265f8ce
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-12-25 11:16:12 -0500

    listo

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function muestras(){
-  document.querySelector('.muestra').style.color = 'green';
+document.querySelector('.muestra').style.color = 'green';
 }
 muestras();
+
+const today = new Date();
+const hourNow = today.getHours();
+let greenting;
+
+if(hourNow > 18){
+  greenting = 'Good evening!'
+} else if(hourNow > 12){
+  greenting = 'Good afternoon!';
+} else if(hourNow > 0){
+ greenting = 'Good morning!';
+} else{
+  greenting = 'welcome';
+}
+const extra = 'We have a good discount call us today!';
+document.getElementById('mas').textContent = greenting + ' ' + extra;
